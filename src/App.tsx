8f99c66f6f69057468068ed61c15ca689ca06768
SullyW <18921310798@163.com>
2023-03-12 22:28:56 +0800

    feat: add tasks and render

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { fetch, update } from './api/tasks';
+import { add, fetch, update } from './api/tasks';
 import { Task } from './types';
 
 const App = () => {
   // useState 更新 tasks的state
   // useState 的初始值是数组、对象等复杂的数据类型，需要明确指定泛型类型, 否则： TS2339: Property 'name' does not exist on type 'never'.
   const [tasks, setTasks] = useState<Task[]>([]);
+  const [newTodo, setNewTodo] = useState<string>('');
 
   // useEffect 获取数据，以便能setTask
   useEffect(() => {
@@ -21,19 +22,27 @@ const App = () => {
     };
     update(Number(task.id), updatedTask).then((data) => console.log(data));
     fetch().then((data) => setTasks(data));
-    // setTasks((tasks) => {
-    //   tasks[Number(task.id)] = data;
-    //   return tasks;
-    // };
-    // setTasks((tasks) => {
-    //   tasks[Number(task.id)] = updatedTask;
-    //   return tasks;
-    // });
   }
 
+  const handleClick = () => {
+    add(newTodo.trim()).then((data) => setTasks([...tasks, data]));
+    setNewTodo('');
+  };
   return (
     <div className="App">
       <h3>Todo List</h3>
+      <section>
+        <input
+          type="text"
+          className="todo-input"
+          placeholder="Enter your todo item"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button onClick={handleClick} className="add-btn">
+          +
+        </button>
+      </section>
       <ul>
         {tasks.map(
           (task) =>
