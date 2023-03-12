d773bcc1e4bc08e71d1823dfa5a05ba939525712
SullyW <18921310798@163.com>
2023-03-12 14:54:05 +0800

    feat: render task name

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { fetch } from './api/tasks';
+import { Task } from './types';
 
 const App = () => {
-  return <div>Todo App</div>;
+  // useState 更新 tasks的state
+  // useState 的初始值是数组、对象等复杂的数据类型，需要明确指定泛型类型, 否则： TS2339: Property 'name' does not exist on type 'never'.
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  // useEffect 获取数据，以便能setTask
+  useEffect(() => {
+    fetch().then((data) => setTasks(data));
+    // console.log(tasks);
+  }, []);
+
+  return (
+    <div className="App">
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>{task.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
