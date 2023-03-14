ff555ec598143e1de0257ed8bbe97068c05aa867
SullyW <18921310798@163.com>
2023-03-14 11:12:57 +0800

    feat: implement mocked resolver

---
+++ resolver.js
@@ -0,0 +1,53 @@
+const tasks = [
+  {
+    id: 1,
+    name: 'if the plane goes down',
+    completed: false,
+  },
+  {
+    id: 2,
+    name: 'the airport',
+    completed: false,
+  },
+  {
+    id: 3,
+    name: 'will shut down',
+    completed: true,
+  },
+  {
+    id: 4,
+    name: 'new where to go then',
+    completed: false,
+  },
+  {
+    id: 5,
+    name: 'how far I would go',
+    completed: true,
+  },
+  {
+    id: 6,
+    name: 'random access',
+    completed: false,
+  },
+  {
+    id: 7,
+    name: 'when I pass away',
+    completed: true,
+  },
+  {
+    id: 8,
+    name: 'interesting',
+    completed: false,
+  },
+  {
+    id: 666,
+    name: 'what should be the answer',
+    completed: true,
+  },
+];
+
+export const resolvers = {
+  Query: {
+    tasks: () => tasks,
+  },
+};
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import { ApolloServer } from 'apollo-server';
 import { typeDefs } from './schema.js';
+import { resolvers } from './resolver.js';
 
-const server = new ApolloServer({ typeDefs });
+const server = new ApolloServer({ typeDefs, resolvers });
 server.listen().then(() => {
   console.log(`
     🚀  Server is running!
