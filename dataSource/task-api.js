c669ec8ccabe485e6d60f3dbd348c54c5eb7de3e
SullyW <18921310798@163.com>
2023-03-14 15:05:39 +0800

    feat: add REST datasource

---
+++ dataSource/task-api.js
@@ -0,0 +1,13 @@
+import { RESTDataSource } from 'apollo-datasource-rest';
+
+export class TaskAPI extends RESTDataSource {
+  constructor() {
+    super();
+    this.baseURL = 'http://localhost:8080/';
+  }
+  // 如果在这里我想要给getTask传一个可以为空的参数completed，要怎么做？
+  // 不过好像没什么必要
+  getTasks(completed) {
+    return this.get(`/tasks/?completed=${completed}`);
+  }
+}
